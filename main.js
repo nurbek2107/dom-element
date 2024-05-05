1695337f88973100791751a37e731739a609eff1
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-05-05 12:45:08 +0500

    main
    Signed-off-by: Nurbek <161306161+nurbek2107@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,28 +1,40 @@
 import data from "./data.js";
 
-const ol = document.querySelector("ol")
-
+const div = document.querySelector(".container")
 data.forEach((product) => {
     const {
         id,
         title,
-        
         description,
+        thumbnail,
         price,
         discountPercentage,
         rating,
         brand,
         stock,
         category,
-        thumbnail,
         images,
     } = product
 
-const li = document.createElement("li")
-const h2 = document.createElement("h2")
-const img = document.createElement("img")
+    const img = document.createElement("img")
+    const div1 = document.createElement("div")
+    const p = document.createElement("p")
+    const h3 = document.createElement("h3")
+    const h2 = document.createElement("h2")
 
-h2.textContent = title;
-img.scr = thumbnail;
-img.width = 500;
-})
+    h2.textContent = title;
+    p.textContent = description;
+    h3.textContent = `${product.price}$`;
+
+    img.src = thumbnail;
+    img.width = 500;
+    img.height = 300;
+
+    div1.appendChild(img);  
+    div.appendChild(div1);
+    div1.appendChild(h2);
+    div1.appendChild(h3);
+    div1.appendChild(p);
+
+    console.log(div1)
+}) 
